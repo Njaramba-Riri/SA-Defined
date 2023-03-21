93216271f8a0d0bce63a7632eacb7a747193aa0f
Riri Njaramba <jamesriri03@gmail.com>
2023-03-21 21:20:36 +0300

    update

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+#make package object instance
+app=Flask()
+
+#define routes and app logic
+@app.route("/")
+def index():
+    return "Hey, welcome."
+
+@app.route("/predict")
+def predict():
+    return"Data Scientist, is that you?"
+
+if __name__=="__main__":
+    app.run(debug=True, Host="0.0.0.0", port=3000)
