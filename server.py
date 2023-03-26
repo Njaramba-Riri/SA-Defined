c68c86e08dd104122790424e0720c705313cacb3
Riri <jamesriri03@gmail.com>
2023-03-26 22:54:30 +0300

    update

--- server.py
+++ server.py
@@ -1,16 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, url, request
+from flask.mysqldb import MySql
+
 
 #make package object instance
 app=Flask()
 
+
+app.config[MYSQL_HOST]='localhost'
+app.
 #define routes and app logic
 @app.route("/", method=["GET", "POST"])
 def index():
-    return "Hey, welcome."
+    return render_template("index.html")
 
 @app.route("/predict")
 def predict():
-    return"Data Scientist, is that you?"
+    if request.method=='POST':
+        details=request.form
+        firstname=details['fname']
+        lastname=details['lname']
+
+    return render_template("forms.html")
+
 
 if __name__=="__main__":
     app.run(debug=True, Host="0.0.0.0", port=3000)
